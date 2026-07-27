1df42909a038e657b26578bef390f171c071e3df
Jie <mischiefsora@gmail.com>
2026-07-28 01:37:03 +0800

    將config重新納入

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBBMfACHtbo33b1RzjNEfJAl2rTOdNAuzE",
+  authDomain: "knitting-counter-dabb7.firebaseapp.com",
+  projectId: "knitting-counter-dabb7",
+  storageBucket: "knitting-counter-dabb7.firebasestorage.app",
+  messagingSenderId: "914715073456",
+  appId: "1:914715073456:web:ecb537d693bb8cb5e8d475",
+  measurementId: "G-RQYZQNS07T"
+};
